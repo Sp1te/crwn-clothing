a2c1cf7febaa63f692516e3b7f43cd38f997859e
Mykhailo Shpytko <mykhailoshpytko@gmail.com>
2021-03-15 14:51:54 +0200

    add new page shop and add a few components for this page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.components";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.components";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.components.jsx
+++ src/components/directory/directory.components.jsx
@@ -12,13 +12,13 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
-          linkUrl: "shop/hats",
+          linkUrl: "hats/",
         },
         {
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
-          linkUrl: "shop/jackets",
+          linkUrl: "jackets",
         },
         {
           title: "sneakers",
@@ -47,8 +47,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.section.map(({ title, id, imageUrl, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.section.map(({ id, ...otherProps }) => (
+          <MenuItem key={id} {...otherProps} />
         ))}
       </div>
     );
